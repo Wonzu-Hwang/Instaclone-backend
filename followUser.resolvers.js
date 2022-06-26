1ebd029c27d0ee6ffd9d0f7302866d410aef1c41
Wonzu-Hwang <107249320+Wonzu-Hwang@users.noreply.github.com>
2022-06-26 12:44:35 +0900

    Add files via upload

---
+++ followUser.resolvers.js
@@ -0,0 +1,16 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
+      await client.users.update({
+        where: { id: loggedInUser.id },
+        data: { following: { connect: { username } } },
+      });
+      return {
+        ok: true,
+      };
+    }),
+  },
+};
