c20e6343c2162cd0e3bfaeae8a0b320748d27934
Wonzu Hwang <dghkddnjswn@gmail.com>
2022-06-26 13:45:26 +0900

    4.21

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,42 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, userName, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                userName,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/Password is already taken");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        //await를 쓰지 않고 return 써도 브라우저는 기다려준다
+        return client.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+};
