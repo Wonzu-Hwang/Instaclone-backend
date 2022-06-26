2d40a7a229c3d416dd20d185562db6cbc5b3ddf7
Wonzu Hwang <dghkddnjswn@gmail.com>
2022-06-26 13:58:26 +0900

    4.21

--- client.js
+++ client.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
 export default client;
-=======
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
-
-export default client;
->>>>>>> db08b248414ef74ef38b35b766aefd424c7d1c41
